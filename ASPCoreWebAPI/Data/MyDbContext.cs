a7c288bdd9dd7a9f49d55c7f9ff4818210316295
Masud Parvez <masud.parvez@brainstation.com>
2024-11-16 21:48:40 +0600

    Add project files.

---
+++ ASPCoreWebAPI/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ASPCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPI.Data;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
---
+++ ASPCoreWebAPI/Program.cs
@@ -0,0 +1,47 @@
+using ASPCoreWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure Entity Framework with SQL Server
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+// Add CORS policy to allow requests from Angular app
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Allow Angular app origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Apply the CORS policy
+app.UseCors("AllowAngularApp");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
